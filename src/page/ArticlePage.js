df456be6925c193c5262540d5e812f314f8b09e8
OzanOcak <ozanocak@gmail.com>
2022-09-28 10:19:55 -0400

    useParams

--- src/page/ArticlePage.js
+++ src/page/ArticlePage.js
@@ -1,5 +1,20 @@
+import { useParams } from "react-router-dom";
+import articles from "../article-content.js";
+
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  // const params = useParams(); // the hook gets the last part of the url
+  // const articleId = params.articleId;
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1>{articleId.title}</h1>
+      {article.content.map((paragraph) => (
+        <p>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
